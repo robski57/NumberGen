4a2b9e19554d758455b67e24e0f533286af137c2
robski57 <robski577@gmail.com>
2016-03-25 14:21:24 -0500

    Added files via upload

---
+++ randomNumGen.py
@@ -0,0 +1,18 @@
+import random
+higher_value = 12
+lower_value = 1
+final_value = random.randint(lower_value, higher_value)
+print(final_value)
+
+
+
+higher_value1 = 12
+lower_value1 = 1
+final_value1 = random.randint(lower_value1, higher_value1)
+print(final_value1)
+
+      
+higher_value2 = 12
+lower_value2 = 1
+final_value2 = random.randint(lower_value2, higher_value2)
+print(final_value2)
